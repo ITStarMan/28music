ea1471094060c45f605cedee71c367026f280b88
duanchao5 <duanchao5@wanda.cn>
2017-03-20 15:28:09 +0800

    新建页面

---
+++ src/components/pages/pg_second.vue
@@ -0,0 +1,22 @@
+<template>
+  <com-header></com-header>
+</template>
+<script>
+import header from '../common/com_header'
+
+export default{
+  data () {
+    return {
+    }
+  },
+  ready: function () {
+  },
+  methods: {
+  },
+  components: {
+    'com-header': header
+  }
+}
+</script>
+<style>
+</style>
--- src/routerMap.js
+++ src/routerMap.js
@@ -3,6 +3,11 @@ const routers = {
     component (resolve) {
       require(['./components/pages/pg_index.vue'], resolve)
     }
+  },
+  '/second': {
+    component (resolve) {
+      require(['./components/pages/pg_second.vue'], resolve)
+    }
   }
 }
 export default routers
