49d4039ee9e7287daf516647bbc84ff50be47ece
DuanChao1 <15032462567@163.com>
2017-03-22 23:19:07 +0800

    列表页接口

---
+++ src/api/getAllGoods.php
@@ -0,0 +1,33 @@
+<?php
+header("content-type:text/json;charset=utf-8");
+header("Access-Control-Allow-Origin: *");
+include("mysql.php");
+$result =mysql_query("select * from goods");
+$data =mysql_query("select * from label");
+$aa=array();
+$cc=array();
+while($row = mysql_fetch_array($data)){
+	$cc[]=$row;
+}
+while($row = mysql_fetch_array($result)){
+	$aa[]=$row;
+}
+$arrlength=count($aa);
+$arrlen=count($cc);
+$bb=array();
+for($x=0;$x<$arrlen;$x++) {
+	$bb[$x]["labelID"] = $cc[$x]["labelID"];
+	$bb[$x]["labelName"] = $cc[$x]["labelName"];
+	$bb[$x]["labelDesc"] = $cc[$x]["labelDesc"];
+	$i=0;
+	$dd=array();
+	for($y=0;$y<$arrlength;$y++) {		
+		if($aa[$y]["labelID"] == $cc[$x]["labelID"]){
+			$dd[$i] = $aa[$y];
+			$i++;
+		}
+	}
+	$bb[$x]["goodsList"] = $dd;
+}
+echo json_encode($bb);
+?>
